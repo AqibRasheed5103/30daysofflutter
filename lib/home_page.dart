8f2d4f20f9925b8d36992a0310899eb0e5515264
Aqib <aqiiibkhannn@gmail.com>
2022-03-27 18:32:15 +0500

    day 2 competed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_catalog/home_page.dart';
 
 class Homepage extends StatelessWidget {
-  const ({ Key? key }) : super(key: key);
+  Homepage({Key? key}) : super(key: key);
 
   final double days = 50;
   final String name = "Aqib";
 
   @override
   Widget build(BuildContext context) {
-    return  Material(
-          child: Container(
-            child: Center(child: Text('tutorial $days days by $name')),
-      )
-    )
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog Application"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome to $days days by $name"),
+        ),
+      ),
+      drawer: Drawer(),
     );
   }
 }
